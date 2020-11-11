0eda2e6581d7cb947134cb42a15f73172a2c3290
pmartinsn <pamela.illisse@gmail.com>
2020-11-11 13:16:39 -0300

    zero and precision with a before and after number, includin negative

--- dealing_zero.c
+++ dealing_zero.c
@@ -6,7 +6,7 @@
 /*   By: pmartins <pmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 09:17:10 by pmartins          #+#    #+#             */
-/*   Updated: 2020/11/11 13:10:26 by pmartins         ###   ########.fr       */
+/*   Updated: 2020/11/11 13:16:08 by pmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,7 @@ void	dea_zero_two(char *tobeconv, const char *fmt, int *aux, t_bdr *star)
 			if (fmt[*aux] == 's')
 			{
 				*aux = *aux + 1;
-				pri_min_pre_wnbr(tobeconv, tbvv, &*star);
+				pri_pre_wnbr(tobeconv, tbvv, &*star);
 				
 			}
 			else if ((fmt[*aux] == 'd') | (fmt[*aux] == 'i'))
