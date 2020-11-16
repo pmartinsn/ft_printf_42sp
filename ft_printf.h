d2c62a43268d7a386c9992cea119faa2be1d4e1d
pmartinsn <pamela.illisse@gmail.com>
2020-11-16 12:24:32 -0300

    changes in printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pmartins <pmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 13:49:45 by pmartins          #+#    #+#             */
-/*   Updated: 2020/11/16 12:02:25 by pmartins         ###   ########.fr       */
+/*   Updated: 2020/11/16 12:16:51 by pmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -142,5 +142,6 @@ void	p_s_pre_width(t_bdr *star);
 void	min_s_pre_ww(int j, int i, t_bdr *star);
 void	p_int_wid_zero(t_bdr *star);
 void	p_int_wid_pre(t_bdr *star);
+void	s_pre_width(t_bdr *star);
 
 #endif
