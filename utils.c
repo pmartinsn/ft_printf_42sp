e373245a3dc9d9e46a01f7a5adcb23c5998da2d1
pmartinsn <pamela.illisse@gmail.com>
2020-11-23 11:41:10 -0300

    trying to fix wrong behavior

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: pmartins <pmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 16:46:50 by pmartins          #+#    #+#             */
-/*   Updated: 2020/11/23 10:54:53 by pmartins         ###   ########.fr       */
+/*   Updated: 2020/11/23 11:40:49 by pmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,6 +89,7 @@ void	p_uxxx_starpstar(int *aux, int c, char a, t_bdr *star)
 
 	help = 0;
 	j = 0;
+	star->hold = 0;
 	j = va_arg(star->list, int);
 	star->hold = va_arg(star->list, int);
 	num = va_arg(star->list, size_t);
