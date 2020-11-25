7b0b8447ed6173e7f4767f338b41f808b8e97c0e
pmartinsn <pamela.illisse@gmail.com>
2020-11-25 18:52:26 -0300

    changes in itoa based conversions

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: pmartins <pmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 10:44:17 by pmartins          #+#    #+#             */
-/*   Updated: 2020/11/25 18:06:38 by pmartins         ###   ########.fr       */
+/*   Updated: 2020/11/25 18:52:10 by pmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char			*ft_itoa_base(size_t nb, int base, char updown, t_bdr *star)
 	aux = nb;
 	while ((aux = aux / base) > 0)
 		cont_num++;
-	str = (char *)ft_calloc((cont_num + 1), sizeof(char));
+	str = (char *)malloc((cont_num + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
 	str[cont_num--] = '\0';
