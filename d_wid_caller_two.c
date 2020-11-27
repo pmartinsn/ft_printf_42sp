0b3ed5952e373800e5ca42d84b13e7cfad2d276b
pmartinsn <pamela.illisse@gmail.com>
2020-11-27 16:13:43 -0300

    changes .*

--- d_wid_caller_two.c
+++ d_wid_caller_two.c
@@ -6,7 +6,7 @@
 /*   By: pmartins <pmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 18:06:16 by pmartins          #+#    #+#             */
-/*   Updated: 2020/11/27 15:49:58 by pmartins         ###   ########.fr       */
+/*   Updated: 2020/11/27 16:13:10 by pmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,7 +124,7 @@ void	p_wid_pre(t_bdr *star)
 		ft_putnbr(star->kpr8, &*star);
 	}
 	else*/
-	if ((j < 0) && star->kpr8 != 0)
+	if ((j < 0))
 		ft_putnbr(star->kpr8, &*star);//e_pwidpre(j, help, &*star);
 	else if ((j >= 0) && star->kpr8 != 0)
 		ft_putnbr(star->kpr8, &*star);
