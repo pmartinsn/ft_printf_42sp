431fb5504fc09292fca4c3d297337a224456b50f
pmartinsn <pamela.illisse@gmail.com>
2020-11-28 11:21:25 -0300

    try seven

--- dealing_zero.c
+++ dealing_zero.c
@@ -6,7 +6,7 @@
 /*   By: pmartins <pmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 09:17:10 by pmartins          #+#    #+#             */
-/*   Updated: 2020/11/28 11:17:53 by pmartins         ###   ########.fr       */
+/*   Updated: 2020/11/28 11:21:15 by pmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,12 +140,12 @@ void	e_starprei(int help, t_bdr *star)
 	}
 	else if (star->kpr2 >= star->kpr5)
 		ft_putnbr(star->kpr4, &*star);
-	help = 0;
+	/*help = 0;
 	while (help < star->kpr2)
 	{
 		ft_putchar(' ', &*star);
 		help++;
-	}
+	}*/
 }
 
 
