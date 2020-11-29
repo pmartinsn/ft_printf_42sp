65a1a2aae74177d6306c054ebbfea19953c277df
pmartinsn <pamela.illisse@gmail.com>
2020-11-29 10:06:18 -0300

    all logical on dealing zero width

--- dealing_zero.c
+++ dealing_zero.c
@@ -6,7 +6,7 @@
 /*   By: pmartins <pmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 09:17:10 by pmartins          #+#    #+#             */
-/*   Updated: 2020/11/29 10:04:11 by pmartins         ###   ########.fr       */
+/*   Updated: 2020/11/29 10:06:15 by pmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -200,7 +200,7 @@ void	e_starprei(int help, t_bdr *star)
 		}
 		help = 0;
 		star->kpr3 = star->kpr3 - star->kpr5 -1;
-		while(help < star->kpr5)
+		while(help < star->kpr3)
 		{
 			ft_putchar(' ', &*star);
 			help++;
@@ -219,7 +219,7 @@ void	e_starprei(int help, t_bdr *star)
 		ft_putnbr(star->kpr4, &*star);
 		help = 0;
 		star->kpr3 = star->kpr3 - star->kpr5;
-		while(help < star->kpr5)
+		while(help < star->kpr3)
 		{
 			ft_putchar(' ', &*star);
 			help++;
